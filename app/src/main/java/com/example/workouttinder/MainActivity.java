6c6d58ba90d0373d3fe0a1f8c2efa4d89d220f57
situyangchen <situyangchen@wustl.edu>
2019-03-02 09:19:55 -0600

    p

--- app/src/main/java/com/example/workouttinder/MainActivity.java
+++ app/src/main/java/com/example/workouttinder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.workouttinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -62,7 +63,8 @@ public class MainActivity extends AppCompatActivity {
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                Intent intent= new Intent(MainActivity.this,UserPage.class)
+                startActivity(intent);
             }
         });
     }

553cb773bf24126286d2036d5596210a46497afc
situyangchen <situyangchen@wustl.edu>
2019-03-02 18:02:02 -0600

    a

--- app/src/main/java/com/example/workouttinder/MainActivity.java
+++ app/src/main/java/com/example/workouttinder/MainActivity.java
@@ -2,17 +2,17 @@ package com.example.workouttinder;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+//import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.app.Activity;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.view.View;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.app.Activity;
+//import android.graphics.Color;
+//import android.os.Bundle;
+//import android.view.View;
 
 import android.widget.Button;
 import android.widget.EditText;
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this,UserPage.class)
+                Intent intent= new Intent(MainActivity.this,ChatActivity.class);
                 startActivity(intent);
             }
         });

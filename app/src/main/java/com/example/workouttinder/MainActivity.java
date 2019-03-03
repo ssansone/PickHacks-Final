637ab98a0f1fd9e4023f15e737bac876049780d9
situyangchen <situyangchen@wustl.edu>
2019-03-02 19:26:47 -0600

    fix3

--- app/src/main/java/com/example/workouttinder/MainActivity.java
+++ app/src/main/java/com/example/workouttinder/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.workouttinder;
 
-import android.content.Intent;
 import android.os.Bundle;
-//import android.support.design.widget.FloatingActionButton;
-//import android.support.design.widget.Snackbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-//import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-//import android.view.Menu;
-//import android.view.MenuItem;
-//import android.app.Activity;
-//import android.graphics.Color;
-//import android.os.Bundle;
-//import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.graphics.Color;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,33 +24,53 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1,b2;
+    Button b2;
     EditText ed1,ed2;
 
-    TextView tx1;
+
+//    TextView tx1;
 //    int counter = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1 = (Button)findViewById(R.id.button);
+        Button b1 = (Button)findViewById(R.id.button);
         ed1 = (EditText)findViewById(R.id.editText);
         ed2 = (EditText)findViewById(R.id.editText2);
 
         b2 = (Button)findViewById(R.id.button2);
+
 //        tx1 = (TextView)findViewById(R.id.textView3);
 //        tx1.setVisibility(View.GONE);
 
+//        b1.setOnClickListener(new OnClickListener() {
+//
+//
+//            public void onClick(View arg) {
+////                Intent viewIntent =
+////                        new Intent("android.intent.action.VIEW",
+////                                Uri.parse("https://google.com"));
+////                startActivity(viewIntent);'
+//                System.out.println("hey");
+//            }
+//        });
+
+
         b1.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
                 if(ed1.getText().toString().equals("admin") &&
                         ed2.getText().toString().equals("admin")) {
                     Toast.makeText(getApplicationContext(),
                             "Redirecting...",Toast.LENGTH_SHORT).show();
-                }else{
+
+                }
+                else {
                     Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
 
 //                            tx1.setVisibility(View.VISIBLE);
 //                    tx1.setBackgroundColor(Color.RED);
@@ -57,17 +81,35 @@ public class MainActivity extends AppCompatActivity {
 //                        b1.setEnabled(false);
 //                    }
                 }
+
+                Intent intent = new Intent(MainActivity.this, Match.class);
+                startActivity(intent);
             }
+
         });
 
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this,ChatActivity.class);
+                Toast.makeText(getApplicationContext(),
+                        "Redirecting...",Toast.LENGTH_SHORT).show();
+                finish();
+
+                Intent intent = new Intent(MainActivity.this, CreateAccount.class);
                 startActivity(intent);
             }
         });
     }
+
+//        final Button button=(Button)findViewById(R.id.loginbtn);
+//            button.setOnClickListener(new View.OnClickListener()) {
+//        @Override
+//        public void onClick(View v)
+//    }
+//
+//    Intent launchactivity= new Intent(MainActivity.this,Details.class);
+//    startActivity(launchactivity);
+
 
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
